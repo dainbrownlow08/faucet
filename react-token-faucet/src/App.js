c28de6a0908e14eb3be972b3b47edd788f58a901
dainbrownlow08 <dain.brownlow@outlook.com>
2021-06-06 22:04:45 -0700

    balance updates

--- react-token-faucet/src/App.js
+++ react-token-faucet/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import FCTToken from "./artifacts/contracts/FCTToken.sol/FCTToken.json";
+import Faucet from "./components/Faucet.js";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
+  const Token = FCTToken;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col>
+            <div>our faucet</div>
+          </Col>
+          <Col>
+            <div> our send area</div>
+          </Col>
+          <Faucet tokenContract={Token} />
+        </Row>
+      </Container>
     </div>
   );
 }
